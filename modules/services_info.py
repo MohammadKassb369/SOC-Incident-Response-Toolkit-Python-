62936c7b6f7dd1d1b4b22ef0f4eb89342ea29fe9
MohammadKassab369 <109513265+MohammadKassb369@users.noreply.github.com>
2025-05-10 15:20:44 +0300

    Update services_info.py

--- modules/services_info.py
+++ modules/services_info.py
@@ -1,1 +1,13 @@
+# modules/services_info.py
+import subprocess
 
+def collect(output_folder):
+    """Collect Windows services information."""
+    filename = f"{output_folder}/services_info.txt"
+    with open(filename, "w") as f:
+        try:
+            output = subprocess.check_output("sc query type= service state= all", shell=True, text=True)
+            f.write(output)
+        except Exception:
+            f.write("Unable to query services.\n")
+    print("[+] Services info collected.")
