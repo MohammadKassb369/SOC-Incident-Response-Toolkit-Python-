26266560630a55ba581c5a1cf8ce2d4439af18a1
MohammadKassab369 <109513265+MohammadKassb369@users.noreply.github.com>
2025-05-10 15:20:04 +0300

    Update process_info.py

--- modules/process_info.py
+++ modules/process_info.py
@@ -1,1 +1,10 @@
+# modules/process_info.py
+import psutil
 
+def collect(output_folder):
+    """Collect running process information."""
+    filename = f"{output_folder}/process_info.txt"
+    with open(filename, "w") as f:
+        for proc in psutil.process_iter(['pid', 'name', 'username']):
+            f.write(f"{proc.info}\n")
+    print("[+] Process list collected.")
