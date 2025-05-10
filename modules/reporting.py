beb454361b1f9b5c65a9eac270a290490566e0b6
MohammadKassab369 <109513265+MohammadKassb369@users.noreply.github.com>
2025-05-10 15:23:07 +0300

    Update reporting.py

--- modules/reporting.py
+++ modules/reporting.py
@@ -1,1 +1,14 @@
+# modules/reporting.py
+import datetime
+import platform
+import socket
 
+def collect(output_folder):
+    """Generate summary report."""
+    filename = f"{output_folder}/triage_summary.txt"
+    with open(filename, "w") as f:
+        f.write("SOC Toolkit - Triage Summary Report\n")
+        f.write(f"Date: {datetime.datetime.now()}\n")
+        f.write(f"Host: {socket.gethostname()}\n")
+        f.write(f"OS: {platform.system()} {platform.release()}\n")
+    print("[+] Triage summary report generated.")
