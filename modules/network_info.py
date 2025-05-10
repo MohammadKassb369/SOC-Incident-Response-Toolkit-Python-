358d783abdb26c6c2873b9504aa1969b65850575
MohammadKassab369 <109513265+MohammadKassb369@users.noreply.github.com>
2025-05-10 15:19:39 +0300

    Update network_info.py

--- modules/network_info.py
+++ modules/network_info.py
@@ -1,1 +1,11 @@
+# modules/network_info.py
+import psutil
 
+def collect(output_folder):
+    """Collect network information."""
+    filename = f"{output_folder}/network_info.txt"
+    with open(filename, "w") as f:
+        f.write("Network Connections:\n")
+        for conn in psutil.net_connections():
+            f.write(f"{conn}\n")
+    print("[+] Network info collected.")
